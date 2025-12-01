c9d7efc5328829f0f57665677290e7303ec7a2e9
AlissonPolo <alissonjhojanatorres@gmail.com>
2025-12-01 00:44:21 -0500

    correction class principal

--- src/main/java/com/example/Bio_Code/BioCodeApplication.java
+++ src/main/java/com/example/Bio_Code/BioCodeApplication.java
@@ -2,19 +2,11 @@ package com.example.Bio_Code;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import io.github.cdimascio.dotenv.Dotenv;
-import io.github.cdimascio.dotenv.DotenvEntry;
 
 @SpringBootApplication
 public class BioCodeApplication {
 
 	public static void main(String[] args) {
-		Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
-
-		for (DotenvEntry entry : dotenv.entries()) {
-			System.setProperty(entry.getKey(), entry.getValue());
-		}
-
 		SpringApplication.run(BioCodeApplication.class, args);
 	}
 }
